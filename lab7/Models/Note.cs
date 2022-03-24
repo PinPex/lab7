4a7b6e6567115e74d21f561f757b0a35c3dde6b4
PinPex <aleksei-pichnuk0202@mail.ru>
2022-03-24 17:07:30 +0700

    Initial commit

---
+++ lab7/Models/Note.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab7.Models
+{
+    public class Note
+    {
+        public Note(string name, string text, DateTimeOffset date)
+        {
+            Name = name;
+            Text = text;
+            Date = date;
+        }
+        public string Name { get; set; }
+        public string Text { get; set; }
+        public DateTimeOffset Date { get; set; }
+
+        
+
+    }
+}
---
+++ lab7/ViewModels/FirstViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using ReactiveUI;
+using System.Reactive;
+using System.Threading.Tasks;
+using System.Reactive.Linq;
+using lab7.Models;
+using System.Collections.ObjectModel;
+
+namespace lab7.ViewModels
+{
+    public class FirstViewModel : ViewModelBase
+    {
+        public FirstViewModel(List<Note> ItemsList)
+        {
+            ItemsAll = ItemsList;
+            Currentdate = DateTime.Today;
+            changeItems(); 
+        }
+
+        DateTimeOffset currentdate;
+        public DateTimeOffset Currentdate
+        {
+            get 
+            {
+                return currentdate;
+            } 
+            set
+            {
+                this.RaiseAndSetIfChanged(ref currentdate, value);
+                changeItems();
+            }
+        }
+        public void changeItems()
+        {
+            List<Note> Items = new List<Note>();
+            foreach (var item in ItemsAll)
+            {
+                if (item.Date.Equals(Currentdate)) Items.Add(item);
+            }
+            ItemsCurrent = Items;
+        }
+
+        public List<Note> ItemsAll { get; set; }
+        public List<Note> itemscurrent;
+
+        public List<Note> ItemsCurrent
+        {
+            get
+            {
+                return itemscurrent;
+            }
+            set
+            {
+                this.RaiseAndSetIfChanged(ref itemscurrent, value);
+            }
+        }
+
+    }
+
+    
+
+    
+
+
+}
